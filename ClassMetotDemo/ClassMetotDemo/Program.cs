dd2153687479eeb620b842b0c473e8a2d599def6
Ozlem Demirci <ozlemdemrcii@gmail.com>
2021-01-31 21:49:48 +0300

    Main Program

--- ClassMetotDemo/ClassMetotDemo/Program.cs
+++ ClassMetotDemo/ClassMetotDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ClassMetotDemo
 {
@@ -6,7 +7,46 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MusteriManager mng = new MusteriManager();
+            mng.Listele();
+
+            // yeni kayıt ekleme
+            mng.Ekle(new Musteri
+            {
+                Adi = "Elif",
+                SoyAdi = "Yıldırım",
+                Id = 3,
+                Tc = "77548965321"
+            });
+
+            // aynı tc li kayıt ekleme başarısız olması lazım
+            mng.Ekle(new Musteri
+            {
+                Adi = "Yusuf",
+                SoyAdi = "Yıldırım",
+                Id = 4,
+                Tc = "11296666666"
+            });
+
+            mng.Listele();
+            ;
+            mng.Sil("77548965321");
+
+            mng.Listele();
+
+
+            // yeni kayıt ekleme
+            mng.Guncelle(new Musteri
+            {
+                Adi = "Ahmet2",
+                SoyAdi = "Yıldırım2",
+                Tc = "11296666666"
+            });
+
+
+
+            mng.Listele();
         }
     }
 }
+
